5a652f132b343cb8fa7c88c95b42835b73884cb7
wallysonlima <wallyson.n.a.lima@gmail.com>
2022-02-14 13:16:23 -0300

    My first commit

---
+++ wlstockmanager/src/main/java/lima/wallyson/wlstockmanager/WlstockmanagerApplication.java
@@ -0,0 +1,13 @@
+package lima.wallyson.wlstockmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WlstockmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WlstockmanagerApplication.class, args);
+	}
+
+}
---
+++ wlstockmanager/src/test/java/lima/wallyson/wlstockmanager/WlstockmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package lima.wallyson.wlstockmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WlstockmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
